76fb69df2732c4d3aac39242af8235e9e145b9b6
Trymad <dummymail@mail.com>
2024-11-24 16:16:41 +0300

    implement comment entity

--- src/main/java/com/trymad/task_management/model/Comment.java
+++ src/main/java/com/trymad/task_management/model/Comment.java
@@ -1,5 +1,37 @@
 package com.trymad.task_management.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
 public class Comment {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    private String text;
+
+    private LocalDateTime created_at;
+
 }
--- src/main/java/com/trymad/task_management/model/User.java
+++ src/main/java/com/trymad/task_management/model/User.java
@@ -7,6 +7,9 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +20,8 @@ import lombok.Setter;
 @Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
