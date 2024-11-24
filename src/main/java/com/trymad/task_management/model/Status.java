46fc0dbd89946712d2e12026d69b7974a3ab5165
Trymad <dummymail@mail.com>
2024-11-24 16:19:32 +0300

    implement status entity

--- src/main/java/com/trymad/task_management/model/Status.java
+++ src/main/java/com/trymad/task_management/model/Status.java
@@ -1,5 +1,22 @@
 package com.trymad.task_management.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
 public class Status {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String value;
+
 }
